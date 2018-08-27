62946e595fe09691553542ff4b4617559ba7725e
crayfishstudio <37799171+crayfishstudio@users.noreply.github.com>
2018-08-27 17:53:58 +0300

    Ьувшфмуд_дмшм

--- js/zoom.js
+++ js/zoom.js
@@ -254,7 +254,7 @@
 
     $(this._targetImage)
       .one($.support.transition.end, $.proxy(this.dispose, this))
-      .emulateTransitionEnd(300)
+      .emulateTransitionEnd(500)
   }
 
   Zoom.prototype.dispose = function () {
